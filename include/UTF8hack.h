c6e17696630e7f69ff4a4ac4cc84c867061fd964
FloydATC <floyd@atc.no>
2020-02-17 16:15:01 +0100

    started cleaning out some of the worst code

--- include/UTF8hack.h
+++ include/UTF8hack.h
@@ -10,12 +10,10 @@ class UTF8hack
     //~UTF8();
 
     static char* append(int cp, char* result, char* error);
-    static std::vector<int> DEPRECATED_codepoints(const char* str);
     static std::vector<uint32_t> codepoints(const char* str, int strlen);
   protected:
 
   private:
-    static int get_codepoint(const char* str, int offset, int* bytes);
 };
 
 #endif // UTF8_H
--- src/UTF8hack.cpp
+++ src/UTF8hack.cpp
@@ -521,39 +521,6 @@ char* UTF8hack::append(int cp, char* result, char* error)
   return result;
 }
 
-int UTF8hack::get_codepoint(const char* str, int offset, int* bytes)
-{
-  int codepoint = 0xFFFD; // Invalid/unknown
-  int len = strlen(str) - offset; // bytes
-  if (len <= 0) { *bytes=0; return 0; }
-  int i = offset;
-  // Decode UTF8 into integer codepoint
-  unsigned char u0 = str[i+0];
-  if (u0>=0 && u0<=127) {
-    *bytes=1; codepoint = u0; // Plain ASCII
-  } else {
-    if (len<2) { *bytes=1; return codepoint; } // Invalid/incomplete
-    unsigned char u1 = str[i+1];
-    if (u0>=192 && u0<=223) {
-      *bytes=2; codepoint = (u0-192)*64 + (u1-128);
-    } else {
-      if (str[i+0]==0xed && (str[i+1] & 0xa0) == 0xa0) { *bytes=2; return codepoint; } //code points, 0xd800 to 0xdfff
-      if (len<3) { *bytes=2; return codepoint; } // Invalid/incomplete
-      unsigned char u2 = str[i+2];
-      if (u0>=224 && u0<=239) {
-        *bytes=3; codepoint = (u0-224)*4096 + (u1-128)*64 + (u2-128);
-      } else {
-        if (len<4) { *bytes=3; return codepoint; } // Invalid/incomplete
-        unsigned char u3 = str[i+3];
-        if (u0>=240 && u0<=247) {
-          *bytes=4; codepoint = (u0-240)*262144 + (u1-128)*4096 + (u2-128)*64 + (u3-128);
-        }
-      }
-    }
-  }
-  return codepoint;
-}
-
 
 std::vector<uint32_t> UTF8hack::codepoints(const char* str, int strlen)
 {
