4cbe55dd4f554db3957e0f0710574215cee485d3
FloydATC <floyd@atc.no>
2020-03-01 18:15:49 +0100

    Enabled depth test for better visualization

--- src/3D/Ray3D.cpp
+++ src/3D/Ray3D.cpp
@@ -1,23 +1,36 @@
 #include "Ray3D.h"
 
+#include <iostream>
+
+//#define DEBUG_TRACE_RAY
+
 Ray3D::Ray3D()
 {
   //ctor
   this->initialized = false;
+  this->name = "Ray3D";
+#ifdef DEBUG_TRACE_RAY
+  std::cout << "Ray3D" << this << " created" << std::endl;
+#endif
 }
 
 Ray3D::~Ray3D()
 {
   //dtor
+#ifdef DEBUG_TRACE_RAY
+  std::cout << "Ray3D" << this << " destroyed" << std::endl;
+#endif
 }
 
 
 
 void Ray3D::render(ShaderProgram* shader)
 {
-  //std::cout << "Ray3D::render()" << std::endl;
+#ifdef DEBUG_TRACE_RAY
+  std::cout << "Ray3D" << this << "::render()" << std::endl;
+#endif
   bind_vao();
-  glDisable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST);
   if (this->finalized == false) finalize();
   if (this->initialized == false) initialize(shader);
   shader->setColor(this->color);
@@ -27,13 +40,15 @@ void Ray3D::render(ShaderProgram* shader)
   glPointSize(4.0);
   glDrawArrays(GL_LINES, 0, 2);
   glDrawArrays(GL_POINTS, 0, 1);
-  glEnable(GL_DEPTH_TEST);
   unbind_vao();
 }
 
 
 void Ray3D::finalize()
 {
+#ifdef DEBUG_TRACE_RAY
+  std::cout << "Ray3D" << this << "::finalize()" << std::endl;
+#endif
   float vertices[6] = {
     0.0, 0.0, 0.0,
     0.0, 0.0, 10.0
